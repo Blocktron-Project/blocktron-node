b454d2c6a618dff269299e20074dee0586517cf0
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-27 21:42:24 +0530

    index-status route added

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,24 +7,24 @@ const router = express.Router();
 /**
  * GET home page.
  */
-router.get('/', function (req, res, next) {
-  let response = {
-    message: 'Blocktron Node is running',
-    port: port,
-    status_code: res.statusCode,
-    configuration: {
-      process_title: process.title,
-      process_pid: process.pid,
-      environment: env,
-      os: process.platform,
-      cpu_arch: process.arch,
-      process_versions: {
-        node_version: process.versions.node,
-        v8_version: process.versions.v8
+router.get('/', function(req, res, next) {
+   let response = {
+      message: 'Blocktron Node is running',
+      port: port,
+      status_code: res.statusCode,
+      configuration: {
+         process_title: process.title,
+         process_pid: process.pid,
+         environment: env,
+         os: process.platform,
+         cpu_arch: process.arch,
+         process_versions: {
+            node_version: process.versions.node,
+            v8_version: process.versions.v8
+         }
       }
-    }
-  };
-  res.json(response);
+   };
+   res.json(response);
 });
 
 module.exports = router;

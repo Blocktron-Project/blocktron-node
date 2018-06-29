8ca7f07f147e41485e5d62d330bc354ba3fbf75b
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-29 21:07:59 +0530

    updates

--- src/routes/blocktron.js
+++ src/routes/blocktron.js
@@ -5,12 +5,21 @@ const express = require('express');
 const router = express.Router();
 
 /**
+ * blocktron-lib datastructure library
+ */
+const BlocktronLib  = require('blocktron-lib');
+
+class Blocktron extends BlocktronLib{
+    constructor(chain, pendingTransactions ){
+        super(chain, pendingTransactions);
+    }
+}
+
+let blocktron = new Blocktron;
+/**
  * GET blocktron listing.
  */
 router.get('/', function(req, res, next) {
-   let response = {
-      message: 'Hello blocktron'
-   };
-   res.json(response);
+   res.send(blocktron);
 });
 module.exports = router;
--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -8,9 +8,6 @@ const router = express.Router();
  * POST transaction
  */
 router.post('/', function(req, res, next) {
-   let response = {
-      message: 'Hello blocktron'
-   };
-   res.json(response);
+   res.send(req.body);
 });
 module.exports = router;

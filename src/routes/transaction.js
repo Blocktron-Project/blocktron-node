adbce9b652b430ae4677eb92d7b0272582360012
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-30 20:59:56 +0530

    create new transaction route created

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -6,9 +6,28 @@ const express = require('express');
 const router = express.Router();
 
 /**
- * POST transaction
+ * POST a transaction
  */
-router.post('/', function(req, res, next) {
-   res.send(req.body);
+router.post('/', function (req, res, next) {
+    /**
+     * Validate the transaction parameters
+     */
+    if (!req || !req.body || !req.body.amount || !req.body.sender || !req.body.receiver) {
+        throw new Error('Cannot create a transaction without required parameters');
+    }
+    /**
+     * Create a transaction with the request parameters.
+     */
+    const blockIndex = blocktron.createNewTransaction(req.body.amount, req.body.sender, req.body.receiver);
+    
+    /**
+     * Construct the response object and send it
+     */
+    let response = {
+        status: 'success',
+        code: res.statusCode,
+        message: `Transaction will be added to the block: ${blockIndex}`
+    };
+    res.send(response);
 });
 module.exports = router;

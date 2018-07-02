9f2c1db48641f45f5d66eb332c29f134cc5d1b03
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-02 20:51:43 +0530

    test updates

--- test/blocktron-node.spec.js
+++ test/blocktron-node.spec.js
@@ -23,6 +23,21 @@ describe('Test the root path -> get/', () => {
     });
 });
 
+describe('Test the blockchain path -> get/blockchain', () => {
+    test('It should respond with 200 OK', (done) => {
+        request(app).get('/blockchain').then((response) => {
+            expect(response.statusCode).toBe(200);
+            done();
+        });
+    });
+    test('It should respond with blockchain data', (done) => {
+        request(app).get('/blockchain').then((response) => {
+//            expect(typeof response.body.chain).toBe('Array');
+            done();
+        });
+    });
+});
+
 afterAll(() => {
     app.close();
 });

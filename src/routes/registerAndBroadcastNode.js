ff3c05c699bc2533a02f6361dbc3972a62be89cb
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-04 21:11:53 +0530

    register and broadcast node router controller WIP

--- src/routes/registerAndBroadcastNode.js
+++ src/routes/registerAndBroadcastNode.js
@@ -14,7 +14,38 @@ const registerAndBroadcastRouter = express.Router();
  * @param {Callback} middleware - Express middleware callback
  */
 registerAndBroadcastRouter.post('/', (req, res, next) => {
+
+    /**
+     * Get the new node's url from request body
+     */
     const newNodeUrl = req.body.newNodeUrl;
-    res.json(newNodeUrl);
+
+    /**
+     * Check whether the node url is already present in the registry
+     */
+    if (blocktron.isNewNode(newNodeUrl)) {
+
+        /**
+         * If url is new, push the url to registry
+         */
+        blocktron.networkNodes.push(newNodeUrl);
+    } else {
+
+        /**
+         * Set response status to 409 to represent resource conflict
+         */
+        res.status(409);
+
+        /**
+         * Construct response
+         */
+        let response = {
+            status: 'resource conflict',
+            code: res.statusCode,
+            message: `Given node url: ${newNodeUrl}, is already present in registry.`
+        };
+        res.json(response);
+    }
 });
+
 module.exports = registerAndBroadcastRouter;

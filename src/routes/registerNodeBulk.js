0b50e468a7de27657f9b264c0e59eb372d141dbb
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-04 22:42:19 +0530

    route setup

--- src/routes/registerNodeBulk.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const registerNodeBulkRouter = express.Router();
-
-module.exports = registerNodeBulkRouter;
---
+++ src/routes/registerNodesBulk.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const registerNodesBulkRouter = express.Router();
+
+module.exports = registerNodesBulkRouter;

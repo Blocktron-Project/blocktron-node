321e9ec6fbe5df4f2c36ca7d3cb60e52b29cb62c
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-04 22:42:28 +0530

    route completed

--- src/routes/registerAndBroadcastNode.js
+++ src/routes/registerAndBroadcastNode.js
@@ -6,6 +6,15 @@ const express = require('express');
 const registerAndBroadcastRouter = express.Router();
 
 /**
+ * The simplified HTTP request client 'request' with Promise support. 
+ * Powered by Bluebird.
+ * `request-promise` returns regular Promises/A+ compliant promises 
+ * and can be assimilated by any compatible promise library.
+ * @see {@link https://www.npmjs.com/package/request-promise|Request-Promise}
+ */
+const request = require('request-promise');
+
+/**
  * POST a url to register and broadcast it to other nodes
  * @function
  * @name post/registerAndBroadcast
@@ -37,7 +46,11 @@ registerAndBroadcastRouter.post('/', (req, res, next) => {
         res.status(409);
 
         /**
-         * Construct response
+         * Construct response and send it
+         * @type {Object}
+         * @param {String} status - The status of the operation 
+         * @param {Number} code - The HTTP response status code
+         * @param {String} message - The message string
          */
         let response = {
             status: 'resource conflict',
@@ -46,6 +59,96 @@ registerAndBroadcastRouter.post('/', (req, res, next) => {
         };
         res.json(response);
     }
+
+    /**
+     * Array to hold the promise objects of nodes registration
+     */
+    let registerNodesPromises = [];
+
+    /**
+     * Register each node url in the networkNodes array
+     */
+    blocktron.networkNodes.forEach(networkNodeUrl => {
+
+        /**
+         * Construct the options for request-promise
+         * @type {Object}
+         * @param {String} uri - The uri to request to
+         * @param {String} method - The HTTP method to use
+         * @param {Object} body - The request body object
+         * @param {Boolean} json - The type of request body
+         */
+        let requestOptions = {
+            uri: networkNodeUrl + '/registerNode',
+            method: 'POST',
+            body: {
+                newNodeUrl: newNodeUrl
+            },
+            json: true
+        };
+
+        /**
+         * Push each promise object returned from `request` into the promise array
+         */
+        registerNodesPromises.push(request(requestOptions));
+    });
+
+    /**
+     * Resolve all promises sequentially and then register them in bulk
+     */
+    Promise.all(registerNodesPromises)
+        .then(data => {
+
+            /**
+             * Construct the bulk registration request and send it
+             * @type {Object}
+             * @param {String} uri - The uri to request to
+             * @param {String} method - The HTTP method to use
+             * @param {Object} body - The request body object
+             * @param {Boolean} json - The type of request body
+             */
+            let bulkRegisterOptions = {
+                uri: newNodeUrl + '/registerNodesBulk',
+                method: 'POST',
+                body: {
+                    allNetworkNodes: [...blocktron.networkNodes, blocktron.currentNodeUrl]
+                },
+                json: true
+            };
+
+            /**
+             * Return the bulk registration promise object
+             */
+            return request(bulkRegisterOptions);
+        })
+        .then(data => {
+
+            /**
+             * Once bulk registration is resolved set appropriate header and send response
+             */
+            res.status(201);
+
+            /**
+             * Construct the reponse and send it
+             * @type {Object}
+             * @param {String} status - The status of the operation 
+             * @param {Number} code - The HTTP response status code
+             * @param {String} message - The message string
+             */
+            let response = {
+                status: 'success',
+                code: res.statusCode,
+                message: 'New node registered with the network'
+            };
+            res.json(response);
+        })
+        .catch((error) => {
+
+            /**
+             * Catch promise reject error
+             */
+            log.error(`Nodes registration failed due to: ${error}`);
+        });
 });
 
 module.exports = registerAndBroadcastRouter;

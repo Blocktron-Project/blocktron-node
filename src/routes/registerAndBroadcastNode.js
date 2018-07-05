99f9fa9560211f7edeebf3e30e8bf107bacd4113
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-05 22:23:14 +0530

    code optimization

--- src/routes/registerAndBroadcastNode.js
+++ src/routes/registerAndBroadcastNode.js
@@ -38,6 +38,99 @@ registerAndBroadcastRouter.post('/', (req, res, next) => {
          * If url is new, push the url to registry
          */
         blocktron.networkNodes.push(newNodeUrl);
+
+        /**
+         * Array to hold the promise objects of nodes registration
+         */
+        let registerNodesPromises = [];
+
+        /**
+         * Register each node url in the networkNodes array
+         */
+        blocktron.networkNodes.forEach(networkNodeUrl => {
+
+            /**
+             * Construct the options for request-promise
+             * @const requestOptions
+             * @type {Object}
+             * @param {String} uri - The uri to request to
+             * @param {String} method - The HTTP method to use
+             * @param {Object} body - The request body object
+             * @param {Boolean} json - The type of request body
+             */
+            let requestOptions = {
+                uri: networkNodeUrl + '/registerNode',
+                method: 'POST',
+                body: {
+                    newNodeUrl: newNodeUrl
+                },
+                json: true
+            };
+
+            /**
+             * Push each promise object returned from `request` into the promise array
+             */
+            registerNodesPromises.push(request(requestOptions));
+        });
+
+        /**
+         * Resolve all promises sequentially and then register them in bulk
+         */
+        Promise.all(registerNodesPromises)
+            .then(data => {
+
+                /**
+                 * Construct the bulk registration request and send it
+                 * @const bulkRegisterOptions
+                 * @type {Object}
+                 * @param {String} uri - The uri to request to
+                 * @param {String} method - The HTTP method to use
+                 * @param {Object} body - The request body object
+                 * @param {Boolean} json - The type of request body
+                 */
+                let bulkRegisterOptions = {
+                    uri: newNodeUrl + '/registerNodesBulk',
+                    method: 'POST',
+                    body: {
+                        allNetworkNodes: [...blocktron.networkNodes, blocktron.currentNodeUrl]
+                    },
+                    json: true
+                };
+
+                /**
+                 * Return the bulk registration promise object
+                 */
+                return request(bulkRegisterOptions);
+            })
+            .then(data => {
+
+                /**
+                 * Once bulk registration is resolved set appropriate header and send response
+                 */
+                res.status(201);
+
+                /**
+                 * Construct the reponse and send it
+                 * @const response
+                 * @type {Object}
+                 * @param {String} status - The status of the operation 
+                 * @param {Number} code - The HTTP response status code
+                 * @param {String} message - The message string
+                 */
+                let response = {
+                    status: 'success',
+                    code: res.statusCode,
+                    message: 'New node registered with the network'
+                };
+                res.json(response);
+            })
+            .catch((error) => {
+
+                /**
+                 * Catch promise reject error
+                 */
+                log.error(`Nodes registration failed due to: ${error}`);
+            });
     } else {
 
         /**
@@ -56,103 +149,10 @@ registerAndBroadcastRouter.post('/', (req, res, next) => {
         let response = {
             status: 'resource conflict',
             code: res.statusCode,
-            message: `Given node url: ${newNodeUrl}, is already present in registry.`
+            message: `Given node url: ${newNodeUrl}, is already present in registry`
         };
         res.json(response);
     }
-
-    /**
-     * Array to hold the promise objects of nodes registration
-     */
-    let registerNodesPromises = [];
-
-    /**
-     * Register each node url in the networkNodes array
-     */
-    blocktron.networkNodes.forEach(networkNodeUrl => {
-
-        /**
-         * Construct the options for request-promise
-         * @const requestOptions
-         * @type {Object}
-         * @param {String} uri - The uri to request to
-         * @param {String} method - The HTTP method to use
-         * @param {Object} body - The request body object
-         * @param {Boolean} json - The type of request body
-         */
-        let requestOptions = {
-            uri: networkNodeUrl + '/registerNode',
-            method: 'POST',
-            body: {
-                newNodeUrl: newNodeUrl
-            },
-            json: true
-        };
-
-        /**
-         * Push each promise object returned from `request` into the promise array
-         */
-        registerNodesPromises.push(request(requestOptions));
-    });
-
-    /**
-     * Resolve all promises sequentially and then register them in bulk
-     */
-    Promise.all(registerNodesPromises)
-        .then(data => {
-
-            /**
-             * Construct the bulk registration request and send it
-             * @const bulkRegisterOptions
-             * @type {Object}
-             * @param {String} uri - The uri to request to
-             * @param {String} method - The HTTP method to use
-             * @param {Object} body - The request body object
-             * @param {Boolean} json - The type of request body
-             */
-            let bulkRegisterOptions = {
-                uri: newNodeUrl + '/registerNodesBulk',
-                method: 'POST',
-                body: {
-                    allNetworkNodes: [...blocktron.networkNodes, blocktron.currentNodeUrl]
-                },
-                json: true
-            };
-
-            /**
-             * Return the bulk registration promise object
-             */
-            return request(bulkRegisterOptions);
-        })
-        .then(data => {
-
-            /**
-             * Once bulk registration is resolved set appropriate header and send response
-             */
-            res.status(201);
-
-            /**
-             * Construct the reponse and send it
-             * @const response
-             * @type {Object}
-             * @param {String} status - The status of the operation 
-             * @param {Number} code - The HTTP response status code
-             * @param {String} message - The message string
-             */
-            let response = {
-                status: 'success',
-                code: res.statusCode,
-                message: 'New node registered with the network'
-            };
-            res.json(response);
-        })
-        .catch((error) => {
-
-            /**
-             * Catch promise reject error
-             */
-            log.error(`Nodes registration failed due to: ${error}`);
-        });
 });
 
 module.exports = registerAndBroadcastRouter;

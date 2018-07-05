4afe54c15f31aaee17e483aaecedd3bd280056c0
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-05 21:53:02 +0530

    register node router set

--- src/routes/registerNode.js
+++ src/routes/registerNode.js
@@ -1,4 +1,61 @@
+/**
+ * blocktron transaction route and controller
+ * @module routers:registerNodeRouter
+ */
 const express = require('express');
 const registerNodeRouter = express.Router();
+
+/**
+ * POST a url to register it with node
+ * @function
+ * @name post/registerNode
+ * @memberof routers:registerNodeRouter
+ * @param {String} path - Express route path
+ * @param {Callback} middleware - Express middleware callback
+ */
+registerNodeRouter.post('/', (req, res, next) => {
+
+    /**
+     * Get the new node's url from request body
+     */
+    const newNodeUrl = req.body.newNodeUrl;
+
+    /**
+     * Check whether current node's url is equal to new node url
+     * if not equal, constant holds a 'true'
+     * @const notCurrentNode
+     * @type {Boolean}
+     */
+    const notCurrentNode = blocktron.currentNodeUrl !== newNodeUrl;
+
+    /**
+     * Check whether node url already exists in data structure, and 
+     * also check whether current node's url is equal to new node url,
+     * then push the new url to networkNodes array
+     */
+    if (blocktron.isNewNode(newNodeUrl) && notCurrentNode) {
+        blocktron.networkNodes.push(newNodeUrl);
+    }
+
+    /**
+     * Set appropriate response status code
+     */
+    res.status(201);
+
+    /**
+     * Construct the reponse and send it
+     * @const response
+     * @type {Object}
+     * @param {String} status - The status of the operation 
+     * @param {Number} code - The HTTP response status code
+     * @param {String} message - The message string
+     */
+    let response = {
+        status: 'success',
+        code: res.statusCode,
+        message: 'New node registered successfully'
+    };
+    res.json(response);
+});
 
 module.exports = registerNodeRouter;

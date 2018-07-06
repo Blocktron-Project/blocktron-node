54dfadef2395f2266bdc3bc553df5bd6dbba776d
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-06 21:47:46 +0530

    route set

--- src/routes/registerNodesBulk.js
+++ src/routes/registerNodesBulk.js
@@ -1,4 +1,26 @@
 const express = require('express');
 const registerNodesBulkRouter = express.Router();
 
+/**
+ * POST multiple node ulrs to register them all at once
+ */
+registerNodesBulkRouter.post('/', (req, res, next) => {
+    const allNetworkNodes = req.body.allNetworkNodes;
+    allNetworkNodes.forEach(networkNodeUrl => {
+        const notCurrentNode = blocktron.currentNodeUrl !== networkNodeUrl;
+        if (blocktron.isNewNode(networkNodeUrl) && notCurrentNode) {
+            blocktron.networkNodes.push(networkNodeUrl);
+        } else {
+            log.error(`${networkNodeUrl} is already present or is a conflicting value`);
+        }
+    });
+    res.status(201);
+    let response = {
+        status: 'success',
+        code: res.statusCode,
+        message: 'Bulk registration of nodes successful'
+    };
+    res.json(response);
+});
+
 module.exports = registerNodesBulkRouter;

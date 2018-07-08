d743a42f1a57b5a5435245ee5e534e6667e4e219
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-08 20:32:05 +0530

    creatNewTransaction override

--- src/lib/blocktron.js
+++ src/lib/blocktron.js
@@ -10,7 +10,12 @@ const BlocktronLib = require('blocktron-lib');
 /**
  * Get the current node's url from command-line args array position 3
  */
-const currentNodeUrl = process.argv[3];
+let currentNodeUrl;
+if (process.argv[3]) {
+  currentNodeUrl = process.argv[3];
+} else {
+  log.info('Node URL argument is missing');
+}
 
 /**
  * The blocktron-lib class enhanced
@@ -56,6 +61,59 @@ class Blocktron extends BlocktronLib {
       return false;
     }
   };
+
+  /**
+   * A method to create a new transaction
+   * @function createNewTransaction
+   * @memberof Blocktron
+   * @param {Number} amount - The amount/value to be recorded
+   * @param {String} sender - The adress of the sender
+   * @param {String} receiver - The address of the receiver
+   * @returns {Object} - Returns the transaction object
+   */
+  createNewTransaction(amount, sender, receiver) {
+
+    /**
+     * Validate the parameters
+     */
+    amount = amount ? amount : (function () {
+      throw new Error('Amount required');
+    })();
+    sender = sender ? sender : (function () {
+      throw new Error('Sender required');
+    })();
+    receiver = receiver ? receiver : (function () {
+      throw new Error('receiver required');
+    })();
+
+    /**
+     * @type {Object}
+     * @const newTransactions - An atomic transactions block in the chain
+     * @property {Number} amount - The value/amount to be recorded
+     * @property {String} sender - The adress of the sender
+     * @property {String} receiver - The address of the receiver
+     */
+    const newTransactions = {
+      amount: amount,
+      sender: sender,
+      receiver: receiver
+    };
+
+    /**
+     * Push the new transaction to the chain
+     */
+    //this.pendingTransactions.push(newTransactions);
+
+    /**
+     * Returns the number of the block, this transaction will be added to
+     */
+    //return this.getLastBlock()['index'] + 1;
+
+    /**
+     * Return the new transaction object
+     */
+    return newTransactions;
+  }
 }
 
 module.exports = Blocktron;

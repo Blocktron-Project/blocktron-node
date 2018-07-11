a3434ed3e89f02bea7cd1e3dfb3a0b7de4ba1bb7
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-11 21:25:09 +0530

    consensus WIP

--- src/routes/consensus.js
+++ src/routes/consensus.js
@@ -7,6 +7,45 @@ const consensusRouter = express.Router();
 
 consensusRouter.get('/', (req, res, next) => {
 
+    /**
+     * Array to hold the request promise objects
+     */
+    let requestPromises = [];
+
+    /**
+     * Broadcast transactions to all nodes in the network node array
+     */
+    blocktron.networkNodes.forEach(networkNodeUrl => {
+
+        /**
+         * Construct the request
+         */
+        const requestOptions = {
+            uri: networkNodeUrl + '/blockchain',
+            method: 'GET',
+            json: true
+        };
+
+        /**
+         * Push request promise objects into request promise array
+         */
+        requestPromises.push(request(requestOptions));
+    });
+
+    /**
+     * Resolve all request promise objects and then send appropriate response
+     */
+    Promise.all(requestPromises)
+
+        /**
+         * Once resolved send the response
+         */
+        .then((blockchains) => {
+
+        })
+        .catch((error) => {
+            log.error(`Consensus check failed due to: ${error}`);
+        });
 });
 
 module.exports = consensusRouter;

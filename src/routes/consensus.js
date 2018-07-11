e3e283794bd3b22cfbaec4474ecd1b2fe5db8b7a
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-11 21:08:55 +0530

    consensus route

---
+++ src/routes/consensus.js
@@ -0,0 +1,12 @@
+/**
+ * Consensus router and controller
+ * @module routers:consensusRouter
+ */
+const express = require('express');
+const consensusRouter = express.Router();
+
+consensusRouter.get('/', (req, res, next) => {
+
+});
+
+module.exports = consensusRouter;

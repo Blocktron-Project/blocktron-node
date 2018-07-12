1d362437cc08afea10538d211f73d5b066e3d865
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-12 20:35:25 +0530

    consensus WIP

--- src/routes/consensus.js
+++ src/routes/consensus.js
@@ -5,6 +5,14 @@
 const express = require('express');
 const consensusRouter = express.Router();
 
+/**
+ * Check for consensus and update blockchains
+ * @function
+ * @name get/consensus
+ * @memberof routers:consensusRouter
+ * @param {String} path - Express route path
+ * @param {Callback} middleware - Express middleware callback
+ */
 consensusRouter.get('/', (req, res, next) => {
 
     /**
@@ -42,6 +50,52 @@ consensusRouter.get('/', (req, res, next) => {
          */
         .then((blockchains) => {
 
+            const currentChainLength = blocktron.chain.length;
+
+            let maximumChainLength = currentChainLength;
+
+            let newLongestChain = null;
+
+            let newPendingTransactions = null;
+
+            blockchains.forEach(blockchain => {
+
+                if (blockchain.chain.length > maximumChainLength) {
+
+                    maximumChainLength = blockchain.chain.length;
+
+                    newLongestChain = blockchain.chain;
+
+                    newPendingTransactions = blockchain.pendingTransactions;
+                }
+            });
+
+            if (!newLongestChain || (newLongestChain && !blocktron.isChainValid(newLongestChain))) {
+
+                res.status(304);
+
+                let response = {
+                    status: 'Not Modified',
+                    code: res.statusCode,
+                    message: 'Current blockchain has not been replaced',
+                    blockchain: blocktron.chain
+                };
+                res.json(response);
+            } else if (newLongestChain && blocktron.isChainValid(newLongestChain)) {
+
+                blocktron.chain = newLongestChain;
+
+                blocktron.pendingTransactions = newPendingTransactions;
+
+                res.status(201);
+
+                let response = {
+                    status: 'Chain replaced',
+                    code: res.statusCode,
+                    message: 'Current blockchain has been replaced',
+                    blockchain: blocktron.chain
+                };
+            }
         })
         .catch((error) => {
             log.error(`Consensus check failed due to: ${error}`);

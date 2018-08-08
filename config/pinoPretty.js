73d590e10f2e9654f4438c0ba830cf69d8588c74
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-08-08 12:36:25 +0530

    updates

--- config/pinoPretty.js
+++ config/pinoPretty.js
@@ -3,15 +3,15 @@
  * @see {@link https://github.com/pinojs/pino-pretty | Pino-Pretty-github}
  */
 const options = {
-    colorize: true, 
-    crlf: true, 
-    errorLikeObjectKeys: ['err', 'error'], 
-    errorProps: '', 
-    levelFirst: false, 
-    localTime: false, 
-    messageKey: 'BTmsg', 
-    translateTime: false, 
-    search: 'foo == `bar`' 
+    colorize: true,
+    crlf: true,
+    errorLikeObjectKeys: ['err', 'error'],
+    errorProps: '',
+    levelFirst: false,
+    localTime: false,
+    messageKey: 'msg',
+    translateTime: false,
+    search: 'foo == `bar`'
 };
 
 module.exports = options;
